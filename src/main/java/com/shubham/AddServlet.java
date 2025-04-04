a0c79aa28fe32c9b0ba04cc03798e775dee29419
Shubham <shubhamatkal@gmail.com>
2025-04-04 12:24:27 +0530

    exception handling in jsp

---
+++ src/main/java/com/shubham/AddServlet.java
@@ -0,0 +1,32 @@
+package com.shubham;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+		PrintWriter out =  res.getWriter();
+		
+		System.out.println("result is "+k);
+		out.println("<html><body bgcolor='cyan'>");
+		out.println("result is "+ k);
+		out.println("</body></html>");
+	}
+
+}
---
+++ src/main/webapp/exception.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" errorPage='error.jsp' %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+//try{
+	int k = 9/0;
+//}
+//catch(Exception e){
+//	out.println("Error "+ e.getMessage());
+//}
+%>
+</body>
+</html>
